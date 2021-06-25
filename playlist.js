b14764860333234334423d5a4fa5ff34fd752e15
andrew <andrew@gmail.com>
2021-06-25 16:14:36 +0800

    all

---
+++ playlist.js
@@ -0,0 +1,12 @@
+let playList = [ 
+    "trDZxtrOZdI", 
+    "Af_DsB0jwh4", 
+    "072tU1tamd0" 
+];
+
+let playTime = 
+[ 
+    [75,95], 
+    [145,172], 
+    [0,7]
+];
---
+++ vue_main.js
@@ -0,0 +1,32 @@
+let pageData = {
+    productName: 'Book a Cruise to the Moon',
+    productDescription: 'Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.',
+    imageSrc:[
+    "images/asteroid.jpg",
+    "images/fantasy.jpg",
+    "images/galaxy.jpg",
+    "images/spaceship.jpg"
+    ],
+    h2ClassController:{
+    centered:true,
+    colorFont:false
+    },
+    pStyleController:{
+    'margin-left':'50px',
+    color:'blue',
+    'font-size':'20px',
+    'font-style':'italic'
+    },
+    imageStyleController:{
+    margin:'auto',
+    display:'block',
+    width:'50%'
+    },
+    imageAlt:"Photo from space"
+};
+const app = Vue.createApp({
+    data(){
+    return pageData;
+    }
+});
+app.mount("#app");
